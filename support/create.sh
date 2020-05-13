1bdd881e654181202dd5c19f51428528d41029db
dinhsan2000 <dinhsan200@gmail.com>
2020-05-13 18:06:18 +0700

    Add Updates OTA Unoffcial Ginkgo Device Sign-off by Dinh San < dinhsan200@gmail.com >

--- support/create.sh
+++ support/create.sh
@@ -1,18 +1,17 @@
-ROMDIR=~/pe
+ROMDIR=~/Plus
 cd $ROMDIR
-DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/jasmine_sprout/system/build.prop | cut -d "=" -f 2)
+DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/ginkgo/system/build.prop | cut -d "=" -f 2)
 FILENAME=$(find out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d "/" -f 5)
-ID=$(md5sum out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d " " -f 1)
+ID=$(md5sum out/target/product/ginkgo/PixelExperience*.zip | cut -d " " -f 1)
 FILEHASH=$ID
-SIZE=$(wc -c out/target/product/jasmine_sprout/PixelExperience*.zip | awk '{print $1}')
-URL1="https://sourceforge.net/projects/private-dir/files/PixelExperience-Fan_Edition/Decrypted/$FILENAME/download"
+SIZE=$(wc -c out/target/product/ginkgo/PixelExperience*.zip | awk '{print $1}')
+URL1="https://sourceforge.net/projects/pixelexperience-ginkgo/$FILENAME/download"
 URL=$URL1
 VERSION="10"
-DONATE_URL="https://www.paypal.me/aryan65"
-WEBSITE_URL="https://sourceforge.net/projects/private-dir/files/PixelExperience-Fan_Edition/Decrypted/"
-NEWS_URL="https:\/\/t.me\/pixeljasmine"
-MAINTAINER="Aryan Gupta"
-MAINTAINER_URL="https:\/\/t.me/boozabooza"
+WEBSITE_URL="https://sourceforge.net/projects/pixelexperience-ginkgo/"
+NEWS_URL="https:\/\/t.me\/rmn8update"
+MAINTAINER="Dinh San"
+MAINTAINER_URL="https:\/\/t.me/san2k"
 FORUM_URL="#"
-JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/jasmine_sprout.json
+JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/ginkgo.json
